243869cd22b273644d7ad08992579ae53870c677
Tom Henderson <tomhenderson@mac.com>
2014-09-19 13:40:47 +1200

    Initial Commit

---
+++ ferry/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ferry
+//
+//  Created by Tom Henderson on 19/09/14.
+//  Copyright (c) 2014 Tom Henderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THXAppDelegate class]));
+    }
+}
